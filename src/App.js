36b31f5a08f9c9b119f4d0f4379d6fbe0515ec57
Mickey Hudson <mickeyh@vt.edu>
2018-12-30 09:52:16 -0500

    Added appsync with AWS mobile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+import Amplify, { graphqlOperation, Auth } from 'aws-amplify';
+import aws_exports from './aws-exports'; //specify location of aws-exports.js file;
+import { Connect, withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
+
+
+
 class App extends Component {
   render() {
-    return (
+    const ListView = ({ events }) => (
       <div>
-        Hello Harriers!
+          <h3>All Events</h3>
+          <ul>
+              {events.map(event => <li key={event.id}>{event.name} ({event.id})</li>)}
+          </ul>
       </div>
     );
+
+    const ListEvents = `query ListEvents {
+      listEvents {
+        items {
+          id
+          name
+          description
+        }
+      }
+    }`;
+
+    return (
+        <Connect query={graphqlOperation(ListEvents)}>
+            {({ data, loading, error }) => {
+                if (error) return (<h3>Error</h3>);
+                if (loading || !data.listEvents) return (<h3>Loading...</h3>);
+                return <ListView events={data.listEvents.items} />
+            }}
+        </Connect>
+    )
   }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
